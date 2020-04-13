df59df35905dddebde824523d2f812a2154dd09f
L TW <lwat413@gmail.com>
2020-04-13 12:58:07 -0500

    Widgets Added.

--- notepad4Me/Program.cs
+++ notepad4Me/Program.cs
@@ -16,7 +16,7 @@ namespace notepad4Me
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
